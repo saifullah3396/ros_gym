8ef58908e2ad41278321759b80d95ffb3239823d
saifullah3396 <Saifullah3396@gmail.com>
2019-12-17 13:55:53 +0500

    Removed previous gazebo connection classes and replaced them with a base ros_robot_env class.

--- src/robot_envs/ros_robot_env.py
+++ src/robot_envs/ros_robot_env.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python
 import rospy
-from openai_ros import robot_gazebo_env
+from gym_gazebo import robot_gazebo_env
+#from gym_airsim import robot_airsim_env
 
+SIMULATION_ENV = robot_gazebo_env.RobotGazeboEnv
 
-class ROSRobotEnv(robot_gazebo_env.RobotGazeboEnv):
+class ROSRobotEnv(SIMULATION_ENV):
     def __init__(self):
-        # list of controllers
-        self.controllers_list = []
-        
         # robot namespace
         self.robot_name_space = ''
 
         # launch connection to gazebo
         super(ROSRobotEnv, self).__init__(
-            controllers_list=self.controllers_list,
             robot_name_space=self.robot_name_space,
-            reset_controls=False,
-            start_init_physics_parameters=True,
-            reset_world_or_sim='WORLD')
-
-        self.gazebo.unpauseSim()
+            update_physics_params_at_start=True)
+        self.sim_handler.unpause()
         self._setup_subscribers()
         self._setup_publishers()
         self._setup_services()
         self._check_all_systems_ready()
         self._setup_services()
-        self.gazebo.pauseSim()
+        self.sim_handler.pause()
 
     def _check_all_systems_ready(self):
         """
@@ -70,7 +65,7 @@ class ROSRobotEnv(robot_gazebo_env.RobotGazeboEnv):
             try:
                 var = rospy.wait_for_message(name, type, timeout)
             except:
-                rospy.logfatal('Sensor topic "%s" is not available. Waiting...', name)
+                rospy.logerror('Sensor topic "%s" is not available. Waiting...', name)
         return var
 
     def _check_publisher_ready(self, name, obj, timeout=5.0):
@@ -80,7 +75,7 @@ class ROSRobotEnv(robot_gazebo_env.RobotGazeboEnv):
         start_time = rospy.Time.now()
         while obj.get_num_connections() == 0 and not rospy.is_shutdown():
             if (rospy.Time.now() - start_time).to_sec() >= timeout:
-                rospy.logfatal('No subscriber found for the publisher %s. Exiting...', name)
+                rospy.logerror('No subscriber found for the publisher %s. Exiting...', name)
     
     def _check_service_ready(self, name, timeout=5.0):
         """
@@ -89,4 +84,4 @@ class ROSRobotEnv(robot_gazebo_env.RobotGazeboEnv):
         try:
             rospy.wait_for_service(name, timeout)
         except (rospy.ServiceException, rospy.ROSException), e:
-            rospy.logfatal("Service %s unavailable.", name)
+            rospy.logerror("Service %s unavailable.", name)
