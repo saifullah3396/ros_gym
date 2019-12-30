be254efdf59dcaf6dc7c364ae48067ba11bd64e6
hassaanhashmi <hassaanhashmi1.0@gmail.com>
2019-12-30 12:04:13 +0500

    Added WorldState class functions for gazebo

--- src/gym_gazebo/robot_gazebo_env.py
+++ src/gym_gazebo/robot_gazebo_env.py
@@ -1,11 +1,19 @@
 import rospy
 import gym
+import numpy as np
 from gym.utils import seeding
 from gazebo_handler import GazeboHandler
 from mavros_gym_msgs.msg import RLExperimentInfo
-from robot_sim_env import RobotSimEnv
+from robot_sim_env import RobotSimEnv, WorldState
 
-class RobotGazeboEnv(RobotSimEnv):
-
+class RobotGazeboEnv(RobotSimEnv, WorldState):
     def __init__(self, robot_name_space, update_physics_params_at_start=True):
         super(RobotGazeboEnv, self).__init__(robot_name_space, GazeboHandler(update_physics_params_at_start))
+        
+    @property
+    def front_camera(self):
+        raise NotImplementedError
+    
+    @property
+    def collision_check(self):
+        raise NotImplementedError
