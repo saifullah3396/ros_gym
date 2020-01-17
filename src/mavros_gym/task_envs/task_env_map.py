80e7a8054069909d83afa3627e6654aacce85e27
saifullah3396 <Saifullah3396@gmail.com>
2020-01-17 15:51:52 +0500

    Fixed the task_env_map naming.

--- src/mavros_gym/task_envs/task_env_map.py
+++ src/mavros_gym/task_envs/task_env_map.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+"""
+This files defines the task map for all the environments that are possible. Any
+new environment must be defined here as <filename>: <classname>
+"""
 
-task_env_map = {
-	'uav_follow_trajectory_task_env_v0' : 'UAVFollowTrajectoryTaskEnv'
+
+TASK_ENV_MAP = {
+    'uav_follow_trajectory_task_env_v0': 'UAVFollowTrajectoryTaskEnv'
 }
