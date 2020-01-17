a9a2d74fe9d1b21d1580bd402e9e1268e62d7c7a
saifullah3396 <Saifullah3396@gmail.com>
2020-01-17 15:13:07 +0500

    Refactored airism provided setup path file according to pylint and changed its automatic usage on import.

--- src/mavros_gym/gym_airsim/setup_path.py
+++ src/mavros_gym/gym_airsim/setup_path.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Airsim setup file provided by airsim.
+"""
 
 # Import this module to automatically setup path to local airsim module
 # This module first tries to see if airsim module is installed via pip
@@ -6,49 +9,49 @@
 # Else we look up grand-parent folder to see if it has airsim folder
 #    and if it does then we add that in sys.path
 
-import os,sys,logging
+import os
+import sys
+import logging
 
-#this class simply tries to see if airsim 
+
 class SetupPath:
+    """
+    This class simply tries to see if airsim exists
+    """
+    # pylint: disable=missing-function-docstring
     @staticmethod
-    def getDirLevels(path):
+    def get_dir_levels(path):
         path_norm = os.path.normpath(path)
         return len(path_norm.split(os.sep))
 
     @staticmethod
-    def getCurrentPath():
+    def get_current_path():
         cur_filepath = __file__
         return os.path.dirname(cur_filepath)
 
     @staticmethod
-    def getGrandParentDir():
-        cur_path = SetupPath.getCurrentPath()
-        if SetupPath.getDirLevels(cur_path) >= 2:
+    def get_grand_parent_dir():
+        cur_path = SetupPath.get_current_path()
+        if SetupPath.get_dir_levels(cur_path) >= 2:
             return os.path.dirname(os.path.dirname(cur_path))
         return ''
 
     @staticmethod
-    def getParentDir():
-        cur_path = SetupPath.getCurrentPath()
-        if SetupPath.getDirLevels(cur_path) >= 1:
+    def get_parent_dir():
+        cur_path = SetupPath.get_current_path()
+        if SetupPath.get_dir_levels(cur_path) >= 1:
             return os.path.dirname(cur_path)
         return ''
 
     @staticmethod
-    def addAirSimModulePath():
-        # if airsim module is installed then don't do anything else
-        #import pkgutil
-        #airsim_loader = pkgutil.find_loader('airsim')
-        #if airsim_loader is not None:
-        #    return
-
-        parent = SetupPath.getParentDir()
-        if parent !=  '':
+    def add_airsim_module_path():
+        parent = SetupPath.get_parent_dir()
+        if parent is not '':
             airsim_path = os.path.join(parent, 'airsim')
             client_path = os.path.join(airsim_path, 'client.py')
             if os.path.exists(client_path):
                 sys.path.insert(0, parent)
         else:
-            logging.warning("airsim module not found in parent folder. Using installed package (pip install airsim).")
-
-SetupPath.addAirSimModulePath()
+            logging.warning(
+                """airsim module not found in parent folder.
+                Using installed package (pip install airsim).""")
