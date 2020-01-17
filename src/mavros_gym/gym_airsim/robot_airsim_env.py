336f1c201590a37b9ed7d5bb689e88fed6781135
saifullah3396 <Saifullah3396@gmail.com>
2020-01-17 15:39:48 +0500

    robot_namespace is removed since it is not used.

--- src/mavros_gym/gym_airsim/robot_airsim_env.py
+++ src/mavros_gym/gym_airsim/robot_airsim_env.py
@@ -7,21 +7,21 @@ from geometry_msgs.msg import PoseStamped, TwistStamped
 
 class RobotAirSimEnv(RobotSimEnv, WorldState):
 
-    def __init__(self, robot_name_space):
-        super(RobotAirSimEnv, self).__init__(robot_name_space, AirsimHandler())
-        
+    def __init__(self):
+        super(RobotAirSimEnv, self).__init__(AirsimHandler())
+
     @staticmethod
     def airsim_to_ros_pose(airsim_position, airsim_orientation):
         ros_pose = PoseStamped()
         ros_pose.pose.position.x = airsim_position.x_val
         ros_pose.pose.position.y = airsim_position.y_val
         ros_pose.pose.position.z = airsim_position.z_val
-        ros_pose.pose.orientation.w = airsim_orientation.w_val  
+        ros_pose.pose.orientation.w = airsim_orientation.w_val
         ros_pose.pose.orientation.x = airsim_orientation.x_val
         ros_pose.pose.orientation.y = airsim_orientation.y_val
         ros_pose.pose.orientation.z = airsim_orientation.z_val
         return ros_pose
-    
+
     @staticmethod
     def airsim_to_ros_twist(airsim_lin_vel, airsim_ang_vel):
         ros_twist = TwistStamped()
@@ -32,18 +32,18 @@ class RobotAirSimEnv(RobotSimEnv, WorldState):
         ros_twist.twist.angular.y = airsim_ang_vel.y_val
         ros_twist.twist.angular.z = airsim_ang_vel.z_val
         return ros_twist
-    
+
     @staticmethod
     def airsim_image_to_numpy(airsim_img_response):
         img1d = np.fromstring(airsim_img_response.image_data_uint8, dtype=np.uint8)
         img_rgb = img1d.reshape(airsim_img_response.height, airsim_img_response.width, 4)
         img_rgb = np.flipud(img_rgb)
         return img_rgb
-    
+
     @property
     def front_camera(self):
         return self.airsim_image_to_numpy(self.sim_handler.client_front_camera)  # from AirSim Handler
-    
+
     @property
     def collision_check(self):
         return self.sim_handler.client_collision_check # from AirSim Handler
