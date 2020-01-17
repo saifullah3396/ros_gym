c8fab7bd4dcebcb798ddc58fd564ef713d26818c
saifullah3396 <Saifullah3396@gmail.com>
2020-01-17 15:40:13 +0500

    Refactoring of gazebo_env.

--- src/mavros_gym/gym_gazebo/robot_gazebo_env.py
+++ src/mavros_gym/gym_gazebo/robot_gazebo_env.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
+"""
+Defines the RobotGazeboEnv class.
+"""
 
-import rospy
-import gym
-import numpy as np
-from gym.utils import seeding
-from mavros_gym_msgs.msg import RLExperimentInfo
 from robot_sim_env import RobotSimEnv, WorldState
 from .gazebo_handler import GazeboHandler
 
+
 class RobotGazeboEnv(RobotSimEnv, WorldState):
+    """
+    The base class for all robots that use gazebo simulator for training.
+    """
     def __init__(self, robot_name_space, update_physics_params_at_start=True):
-        super(RobotGazeboEnv, self).__init__(robot_name_space, GazeboHandler(update_physics_params_at_start))
-        
+        super(RobotGazeboEnv, self).__init__(
+            robot_name_space, GazeboHandler(update_physics_params_at_start))
+
     @property
     def front_camera(self):
+        """
+        Derived from WorldState
+        """
         raise NotImplementedError()
-    
+
     @property
     def collision_check(self):
+        """
+        Derived from WorldState
+        """
         raise NotImplementedError()
